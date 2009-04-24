cea176b7c6deacb302bb72da0d5e8626e90d504d
jarodwen <jarodwen@e817cfcc-2bb7-11de-94d6-134fa40061eb>
2009-04-24 22:08:11 +0000

    delete

--- trunk/Netflix09/csg339/mapreduce/predictor/package-into.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains two attempts on sharing the global information 
- * among different mappers. Since now both of these two methods can not 
- * work smoothly. More future work is necessary to turn them on.
- */
-package csg339.mapreduce.predictor;
--- trunk/Netflix09/csg339/mapreduce/predlearner/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains a working version of the iterative SVD algorithm using
- * hadoop system. 
- */
-package csg339.mapreduce.predlearner;
